dd1d59795c378c651216485f936b4e72b2f6336f
Ashleigh Kelley <31831238+ashleighkelley@users.noreply.github.com>
2021-03-25 15:47:07 -0400

    prod build

--- index.js
+++ index.js
@@ -14,8 +14,10 @@ app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
 
-app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname+'/client/public/index.html'));
+app.use(express.static('client/build'));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 
 
