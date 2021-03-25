e6c986491a0b5e058f73469750425139d9ca2aea
Ashleigh Kelley <31831238+ashleighkelley@users.noreply.github.com>
2021-03-25 15:29:17 -0400

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.listen(PORT, () => {
 });
 
 app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, './client/public/index.html'));
+    res.sendFile(path.join(__dirname, '/client/public/index.html'));
 });
 
 
