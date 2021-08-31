0065bdc56d3a36f83b662d613cf6b7b231f67dd9
Ashleigh Kelley <31831238+ashleighkelley@users.noreply.github.com>
2021-08-31 12:24:11 -0400

    album themed CSS updates

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,13 +1,35 @@
-import React from 'react'
+import React, {useState, useEffect}  from 'react'
 import { Link } from 'react-router-dom'
 import '../App.css'
+//import 'bootstrap/dist/css/bootstrap.css';
+import Dropdown from 'react-bootstrap/Dropdown';
+
+const getLocalTheme = () => {
+  let theme = 'amulet';
+  if (localStorage.getItem('theme')){
+    theme = localStorage.getItem('theme');
+  }
+  return theme;
+}
 
 export default function Navbar() {
+
+  const [theme, setTheme] = useState(getLocalTheme());
+
+  useEffect(() => {
+    document.documentElement.className = theme;
+    localStorage.setItem('theme', theme);
+  }, [theme])
+
+  const toggleTheme = (theme) => {
+    setTheme(theme);
+  }
+
   return (
     <nav className='navbar'>
-      <div className='nav-center'>
+      <div className='nav-left'>
         <div className="title">
-          <h2>Creature Club Book Club</h2>
+          <h2>Creature Club Pagemasters</h2>
         </div>
         {/*<ul className='nav-links'>
           <li>
@@ -17,6 +39,19 @@ export default function Navbar() {
             <Link to='/pastsuggestions'>Past Suggestions</Link>
           </li> 
         </ul>*/}
+        </div>
+        <div class="nav-right">
+        <Dropdown>
+          <Dropdown.Toggle variant="success">Select a Theme</Dropdown.Toggle>
+          <Dropdown.Menu>
+            <Dropdown.Item href="#" onClick={() => toggleTheme('juturna')}>Juturna</Dropdown.Item>
+            <Dropdown.Item href="#" onClick={() => toggleTheme('olg')}>On Letting Go</Dropdown.Item>
+            <Dropdown.Item href="#" onClick={() => toggleTheme('bsn')}>Blue Sky Noise</Dropdown.Item>
+            <Dropdown.Item href="#" onClick={() => toggleTheme('waves')}>Violent Waves</Dropdown.Item>
+            <Dropdown.Item href="#" onClick={() => toggleTheme('descensus')}>Descensus</Dropdown.Item>
+            <Dropdown.Item href="#" onClick={() => toggleTheme('amulet')}>The Amulet</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
       </div>
     </nav>
   )
